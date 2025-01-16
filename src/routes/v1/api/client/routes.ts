44e98634664ec5949460c465cfe80af0d986e8b9
Jovine jestas <jestasjovin@gmail.com>
2025-01-16 19:19:01 +0300

    client routes

---
+++ src/routes/v1/api/client/routes.ts
@@ -0,0 +1,28 @@
+// src/routes/clientRoutes.ts
+import { Router, Request, Response } from "express";
+import authenticate from "../../../../middlewares/authMiddleware";
+import * as clientController from "./controller"; // Import all controller functions
+
+const clientRouter = Router();
+
+// get all clients
+clientRouter.get("/", authenticate, clientController.getAllClients);
+// get client by ID
+clientRouter.get("/:id", authenticate, clientController.getClientById);
+
+// client actions
+const usingClientRouter = Router();
+// update client by ID
+usingClientRouter.put("/:id", authenticate, clientController.updateClient);
+// delete client by ID
+usingClientRouter.delete("/:id", authenticate, clientController.deleteClient);
+clientRouter.use("/using_client", usingClientRouter);
+
+// Sub-router for client registration-related routes
+const registerRouter = Router();
+//  register a new client
+registerRouter.post("/register", authenticate, clientController.registerClient);
+// Mount the registerRouter on `/register` path
+clientRouter.use("/register", registerRouter);
+
+export default clientRouter;
